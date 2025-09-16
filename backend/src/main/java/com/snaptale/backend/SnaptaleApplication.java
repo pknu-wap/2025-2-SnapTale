b24205262fecc8ec0ffad75443c11fe8e7082c2c
Seol-Munhyeok <abc1234568@naver.com>
2025-09-16 19:33:56 +0900

    chore: init monorepo (backend + frontend scaffold)

---
+++ backend/src/main/java/com/snaptale/backend/SnaptaleApplication.java
@@ -0,0 +1,13 @@
+package com.snaptale.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaptaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaptaleApplication.class, args);
+	}
+
+}
