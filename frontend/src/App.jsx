285c20054ded30235c5aac9ba33b5f8692e9434d
mututu17 <161244328+mututu17@users.noreply.github.com>
2025-09-20 20:06:58 +0900

    feat(route): app.jsx라우팅 구조 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Init } from "./Components/Init";
+import { Home } from "./Components/Home";
+import { GamePlay } from "./Components/GamePlay";
+import { GameResult } from "./Components/GameResult";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Router>
+      <Routes>
+        {/* 초기 화면 */}
+        <Route path="/" element={<Init />} />
+        {/* 메인 홈 화면*/}
+        <Route path="/home" element={<Home />} />
+        {/* 게임 플레이 화면 */}
+        <Route path="/gameplay" element={<GamePlay />} />
+        {/* 게임 결과 확인 화면*/}
+        <Route path="/gameresult" element={<GameResult />} />
+      </Routes>
+    </Router>
     </>
   )
 }
