0c42ef00994863b68be5cd0c77696e8e6784aa3a
mututu17 <161244328+mututu17@users.noreply.github.com>
2025-09-22 14:47:39 +0900

    feat(page): 홈 화면 반응형 적용
    아직 부족한 점이 매우 많음

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,18 +1,26 @@
 import Button from "./Button"
 import Profile from "./Profile";
 import Soundbar from "./Soundbar";
+import './Home.css'
 
 const Home = () => {
   return (
-    <>
-      <Soundbar/>
-      <Profile/>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        <li><Button text={"랜덤 매치"} /></li>
-        <li><Button text={"친선전"} /></li>
-        <li><Button text={"튜토리얼"} /></li>
-      </ul>
-    </>
+    <div className="home-container">
+      
+      {/* 상단 메뉴 (사운드바, 프로필) */}
+      <header className="top-menu">
+        <Soundbar />
+        <Profile />
+      </header>
+
+      {/* 메인 버튼 영역 */}
+      <main className="matching-buttons">
+        <Button text={"랜덤 매치"} />
+        <Button text={"친선전"} />
+        <Button text={"튜토리얼"} />
+      </main>
+
+    </div>
   );
 };
 export default Home;
