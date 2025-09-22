3ff52a7120bac24999be7f7f9e1d86bb4d58a4e0
mututu17 <161244328+mututu17@users.noreply.github.com>
2025-09-22 12:45:19 +0900

    feat(page): 스피커 아이콘 컴포넌트 추가

--- frontend/src/Components/Home/Profile.jsx
+++ frontend/src/Components/Home/Profile.jsx
@@ -2,7 +2,10 @@ import userProfile from "../../assets/userProfile.png";
 const Profile = () => {
   return (
     <img
-    src = {userProfile} alt= "User Profile" width={55} height={55}
+        src = {userProfile} 
+        alt= "User Profile" 
+        width={55} 
+        height={55}
     />
   );
 };
