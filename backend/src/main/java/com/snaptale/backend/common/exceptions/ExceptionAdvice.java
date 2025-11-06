325d2a7b5d8fd541e8060e8880f75ecec1deb5ca
gilnyang coding <kdw061224@gmail.com>
2025-11-06 21:41:35 +0900

    fix(back, baseException): 없길래 다시 만듬

--- backend/src/main/java/com/snaptale/backend/common/exceptions/ExceptionAdvice.java
+++ backend/src/main/java/com/snaptale/backend/common/exceptions/ExceptionAdvice.java
@@ -1,10 +1,33 @@
 package com.snaptale.backend.common.exceptions;
 
+import com.snaptale.backend.common.response.BaseResponse;
+import com.snaptale.backend.common.response.BaseResponseStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<BaseResponse<Object>> handleBaseException(BaseException e) {
+        log.error("BaseException 발생: {}", e.getStatus().getMessage(), e);
+        BaseResponse<Object> response = new BaseResponse<>(e.getStatus(), null);
+        return ResponseEntity
+                .status(e.getStatus().getHttpStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponse<Object>> handleException(Exception e) {
+        log.error("예상치 못한 예외 발생", e);
+        BaseResponse<Object> response = new BaseResponse<>(
+                BaseResponseStatus.INTERNAL_SERVER_ERROR,
+                null);
+        return ResponseEntity
+                .status(BaseResponseStatus.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(response);
+    }
 }
