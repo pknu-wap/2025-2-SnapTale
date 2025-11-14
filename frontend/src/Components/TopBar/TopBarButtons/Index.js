621e548b9545979c5d09353ffa00c3c2e72fe78a
mututu17 <161244328+mututu17@users.noreply.github.com>
2025-11-14 19:00:34 +0900

    fix: 중복 export 오류

--- frontend/src/Components/TopBar/TopBarButtons/Index.js
+++ frontend/src/Components/TopBar/TopBarButtons/Index.js
@@ -2,7 +2,3 @@ export { default as DeckIcon } from "./DeckIcon";
 export { default as ExitButton } from "./ExitButton";
 export { default as SoundIcon } from "./SoundIcon";
 export { default as StoreButton } from "./StoreButton";
-export { default as DeckIcon } from "./DeckIcon";
-export { default as ExitButton } from "./ExitButton";
-export { default as SoundIcon } from "./SoundIcon";
-export { default as StoreButton } from "./StoreButton";
